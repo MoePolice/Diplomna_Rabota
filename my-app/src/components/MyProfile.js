d5c3d1a506d992aab564116eeae720dcb7dca5b6
MoePolice <112794494+MoePolice@users.noreply.github.com>
2023-03-14 16:59:41 +0200

    Setting up useStates

--- my-app/src/components/MyProfile.js
+++ my-app/src/components/MyProfile.js
@@ -1,2 +1,9 @@
 import { useState, useEffect } from "react";
 import { firebase, db } from "../firebase";
+
+const MyProfile = () => {
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [displayName, setDisplayName] = useState("");
+  const [bio, setBio] = useState("");
+};
