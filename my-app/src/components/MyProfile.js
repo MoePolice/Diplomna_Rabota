74a3fbc91a8d034844aab16a07ade95b274b672c
MoePolice <112794494+MoePolice@users.noreply.github.com>
2023-03-14 17:09:48 +0200

    exporting myProfile

--- my-app/src/components/MyProfile.js
+++ my-app/src/components/MyProfile.js
@@ -8,63 +8,63 @@ const MyProfile = () => {
   const [bio, setBio] = useState("");
 
   const currentUser = firebase.auth().currentUser;
-};
 
-useEffect(() => {
-  const fetchUserData = async () => {
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        if (!currentUser) {
+          throw new Error("User not authenticated");
+        }
+
+        const userRef = db.collection("users").doc(currentUser.uid);
+        const userDoc = await userRef.get();
+
+        if (userDoc.exists) {
+          setUser(userDoc.data());
+          setDisplayName(userDoc.data().displayName);
+          setBio(userDoc.data().bio);
+        } else {
+          console.log("User does not exist in the database.");
+        }
+
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Error fetching user data: ", error);
+      }
+    };
+
+    fetchUserData();
+  }, [currentUser]);
+
+  const handleDisplayNameChange = (e) => {
+    setDisplayName(e.target.value);
+  };
+
+  const handleBioChange = (e) => {
+    setBio(e.target.value);
+  };
+
+  const handleUpdateProfile = async () => {
     try {
-      if (!currentUser) {
-        throw new Error("User not authenticated");
-      }
-
-      const userRef = db.collection("users").doc(currentUser.uid);
-      const userDoc = await userRef.get();
-
-      if (userDoc.exists) {
-        setUser(userDoc.data());
-        setDisplayName(userDoc.data().displayName);
-        setBio(userDoc.data().bio);
-      } else {
-        console.log("User does not exist in the database.");
-      }
-      setIsLoading(false);
+      await db.collection("users").doc(currentUser.uid).set(
+        {
+          displayName,
+          bio,
+          updatedAt: firebase.firestore.FieldValue.serverTimestamp(),
+        },
+        { merge: true }
+      );
+      console.log("Profile updated successfully.");
     } catch (error) {
-      console.error("Error fetching user data: ", error);
+      console.error("Error updating profile: ", error);
     }
   };
 
-  fetchUserData();
-}, [currentUser]);
-
-const handleDisplayNameChange = (e) => {
-  setDisplayName(e.target.value);
-};
-
-const handleBioChange = (e) => {
-  setBio(e.target.value);
-};
-
-const handleUpdateProfile = async () => {
-  try {
-    await db.collection("users").doc(currentUser.uid).set(
-      {
-        displayName,
-        bio,
-        updatedAt: firebase.firestore.FieldValue.serverTimestamp(),
-      },
-      { merge: true }
-    );
-    console.log("Profile updated successfully.");
-  } catch (error) {
-    console.error("Error updating profile: ", error);
+  if (isLoading) {
+    return <div>Loading...</div>;
   }
-};
 
-if (isLoading) {
-  return <div>Loading...</div>;
-}
-
-return (
+  return (
     <div>
       <h2>My Profile</h2>
       <label htmlFor="displayName">Display Name:</label>
@@ -80,3 +80,5 @@ return (
     </div>
   );
 };
+
+export default MyProfile;
