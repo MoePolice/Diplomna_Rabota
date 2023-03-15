646295f414b549993e30a3d0f6a30ecfe8f898cf
MoePolice <112794494+MoePolice@users.noreply.github.com>
2023-03-15 23:27:18 +0200

    setting UseStates

---
+++ my-app/src/components/searchBar.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+
+const SearchBar = ({ onSearch }) => {
+  const [query, setQuery] = useState("");
+
+
+
+
+export default SearchBar;
