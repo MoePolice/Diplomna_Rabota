d528e2de8c441c0468a3081751ba3780ac3ef249
MoePolice <112794494+MoePolice@users.noreply.github.com>
2023-03-15 23:27:37 +0200

    handling submit

--- my-app/src/components/searchBar.js
+++ my-app/src/components/searchBar.js
@@ -4,7 +4,10 @@ import { Form, Button } from "react-bootstrap";
 const SearchBar = ({ onSearch }) => {
   const [query, setQuery] = useState("");
 
-
-
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSearch(query);
+  };
+};
 
 export default SearchBar;
